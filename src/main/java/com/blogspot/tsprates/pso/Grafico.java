17f2bfd806417a35459cafcf565a80fbb75b1637
Thiago <tsprates@hotmail.com>
2016-04-02 17:44:36 -0300

    refatorando gráfico

--- src/main/java/com/blogspot/tsprates/pso/Grafico.java
+++ src/main/java/com/blogspot/tsprates/pso/Grafico.java
@@ -7,40 +7,45 @@ import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
 import org.knowm.xchart.XYSeries.XYSeriesRenderStyle;
 import org.knowm.xchart.style.Styler.LegendPosition;
+import org.knowm.xchart.style.XYStyler;
 
-public class Grafico 
+public class Grafico
 {
 
     private XYChart chart;
 
-    public void mostra() 
+    public void mostra()
     {
-	new SwingWrapper<XYChart>(chart).displayChart();
+        new SwingWrapper<XYChart>(chart).displayChart();
     }
 
-    public Grafico(String titulo) 
+    public Grafico(String titulo)
     {
-	chart = new XYChartBuilder().width(800).height(600).build();
+        chart = new XYChartBuilder().width(800).height(600).build();
 
-	chart.getStyler().setDefaultSeriesRenderStyle(
-		XYSeriesRenderStyle.Scatter);
-	chart.getStyler().setChartTitleVisible(true);
-	chart.getStyler().setLegendPosition(LegendPosition.OutsideE);
-	chart.getStyler().setMarkerSize(16);
-	chart.setTitle(titulo);
-	chart.setXAxisTitle("Complexidade");
-	chart.setYAxisTitle("Sensibilidade x Especificidade");
-	
-	chart.getStyler().setXAxisMax(1);
-	chart.getStyler().setXAxisMin(0);
-	chart.getStyler().setYAxisMax(1);
-	chart.getStyler().setYAxisMin(0);
+        XYStyler styler = chart.getStyler();
+        
+        styler.setDefaultSeriesRenderStyle(
+                XYSeriesRenderStyle.Scatter);
+        styler.setChartTitleVisible(true);
+        styler.setLegendPosition(LegendPosition.OutsideE);
+        styler.setMarkerSize(16);
+        
+        chart.setTitle(titulo);
+        chart.setXAxisTitle("Complexidade");
+        chart.setYAxisTitle("Sensibilidade x Especificidade");
+
+        styler.setXAxisMax(1);
+        styler.setXAxisMin(0);
+        styler.setYAxisMax(1);
+        styler.setYAxisMin(0);
     }
 
-    public Grafico adicionaSerie(String legenda, List<? extends Number> xData, List<? extends Number> yData) 
+    public Grafico adicionaSerie(String legenda, List<? extends Number> xData,
+            List<? extends Number> yData)
     {
-	chart.addSeries(legenda, xData, yData);
-	return this;
+        chart.addSeries(legenda, xData, yData);
+        return this;
     }
 
 }
