43ea587d1616ba03f310ebe0c18e8ce01bc2be3f
Thiago <tsprates@hotmail.com>
2016-04-13 13:47:45 -0300

    update verificação de args do App

--- src/main/java/com/blogspot/tsprates/pso/App.java
+++ src/main/java/com/blogspot/tsprates/pso/App.java
@@ -13,49 +13,49 @@ import java.util.Properties;
  * @author thiago
  *
  */
-public class App
-{
+public class App {
 
     /**
      * Main.
      *
      * @param args
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("--------------------------------------------");
         System.out.println(" Projeto de Dissertação Mestrado            ");
         System.out.println(" Implementação PSO                          ");
         System.out.println("--------------------------------------------");
-        
-        if (args[0] != null && Files.exists(Paths.get(args[0]))) {
+
+        if (args.length > 0 && Files.exists(Paths.get(args[0]))) 
+        {
             Connection conexaoDb = new DbFactory().conecta();
             Properties config = getConfigs(args[0]);
             Pso pso = new Pso(conexaoDb, config);
 //            pso.mostraPopulacao();
-            pso.carrega();            
-        } else {
+            pso.carrega();
+        } 
+        else 
+        {
             System.err.println("É necessário definir um arquivo de configuração.");
         }
-
 
     }
 
     /**
      * Retorna arquivo de configurações.
-     * 
+     *
      * @param configFile Configurations
      * @return Properties
      */
-    private static Properties getConfigs(String configFile)
+    private static Properties getConfigs(String configFile) 
     {
-        try (FileInputStream fis = new FileInputStream(configFile))
+        try (FileInputStream fis = new FileInputStream(configFile)) 
         {
             Properties prop = new Properties();
             prop.load(fis);
             return prop;
-        }
-        catch (IOException e)
+        } 
+        catch (IOException e) 
         {
             throw new RuntimeException(
                     "Arquivo de configurações não encontrado.", e);
