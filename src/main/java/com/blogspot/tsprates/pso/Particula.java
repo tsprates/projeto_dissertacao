538aadd725c4ad4420c8468e97e51ad20c8efca0
Thiago <tsprates@hotmail.com>
2016-08-22 22:13:00 -0300

    atualiza comparação entre as partículas

--- src/main/java/com/blogspot/tsprates/pso/Particula.java
+++ src/main/java/com/blogspot/tsprates/pso/Particula.java
@@ -213,7 +213,9 @@ public class Particula implements Comparable<Particula>
         {
             if (fitness[0] == pfit[0])
             {
-                return 0;
+                // Caso possuam os mesmos valores de fitness
+                // escolher alfabeticamente pela SQL
+                return this.strPos.compareTo(part.strPos);
             }
             else if (fitness[0] < pfit[0])
             {
