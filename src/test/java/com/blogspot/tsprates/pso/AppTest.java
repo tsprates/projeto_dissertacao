a8970d07fa341d2d63621c07fa4b396237a638b2
Thiago <tsprates@hotmail.com>
2016-09-18 22:46:37 -0300

    adiciona teste Fronteira Pareto

--- src/test/java/com/blogspot/tsprates/pso/AppTest.java
+++ src/test/java/com/blogspot/tsprates/pso/AppTest.java
@@ -1,8 +1,11 @@
 package com.blogspot.tsprates.pso;
 
+import java.util.Arrays;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 
 /**
  * Unit test for simple App.
@@ -35,5 +38,68 @@ public class AppTest
     public void testApp()
     {
         assertTrue(true);
+    }
+
+    public void test_particula_A_domina_particula_B()
+    {
+        double[] afit =
+        {
+            0.4, 0.4, 1
+        };
+
+        double[] bfit =
+        {
+            0.3, 0.4, 1
+        };
+
+        Particula pa = Mockito.mock(Particula.class);
+        when(pa.fitness()).thenReturn(afit);
+
+        Particula pb = Mockito.mock(Particula.class);
+        when(pb.fitness()).thenReturn(bfit);
+
+        assertTrue(FronteiraPareto.verificarDominanciaParticulas(pa, pb) == 1);
+    }
+
+    public void test_particula_A_nao_domina_particula_B()
+    {
+        double[] afit =
+        {
+            0.4, 0.4, 1
+        };
+
+        double[] bfit =
+        {
+            0.3, 0.5, 1
+        };
+
+        Particula pa = Mockito.mock(Particula.class);
+        when(pa.fitness()).thenReturn(afit);
+
+        Particula pb = Mockito.mock(Particula.class);
+        when(pb.fitness()).thenReturn(bfit);
+
+        assertTrue(FronteiraPareto.verificarDominanciaParticulas(pa, pb) == 0);
+    }
+
+    public void test_particula_B_domina_particula_A()
+    {
+        double[] afit =
+        {
+            0.4, 0.4, 1
+        };
+
+        double[] bfit =
+        {
+            0.5, 0.5, 1
+        };
+
+        Particula pa = Mockito.mock(Particula.class);
+        when(pa.fitness()).thenReturn(afit);
+
+        Particula pb = Mockito.mock(Particula.class);
+        when(pb.fitness()).thenReturn(bfit);
+
+        assertTrue(FronteiraPareto.verificarDominanciaParticulas(pa, pb) == -1);
     }
 }
